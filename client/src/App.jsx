9a725d27fc80bf23d2b6c923adc629819f4dcb66
pawantech12 <pawankumavat042@gmail.com>
2024-11-03 18:56:55 +0530

    Added speed insights to website to check or anylize performance of website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App({ element }) {
   return (
     <>
       <main>{element}</main>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
